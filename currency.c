e413046d72af1b34d9ddbe32bd52ad3da3ebdcad
Everton Vanoni Fernandes <86416772+vertocode@users.noreply.github.com>
2021-08-04 22:01:09 -0300

    Update and rename cash.c to currency.c

--- currency.c
+++ currency.c
@@ -15,27 +15,51 @@ int main(void)
     //applying calculation
     d = round(d * 100);
     int n = 0;
+    int n25 = 0;
+    int n10 = 0;
+    int n5 = 0;
+    int n1 = 0;
     while (d > 0)
     {
         if (d >= 25)
         {
             d = d - 25;
+            n25++
         }
         else if (d >= 10)
         {
             d = d - 10;
+            n10++
         }
         else if (d >= 5)
         {
             d = d - 5;
+            n5++
         }
         else
         {
             d = d - 1;
+            n1++
         }
         n++;
     }
 
     //print result
-    printf("%i\n", n);
+    printf("amount of coins: %i\n", n);
+    if(n25 >= 1)
+    {
+        printf("\n 25 cents used: %i\n", n25);
+    }
+    if(n10 >= 1)
+    {
+        printf("\n 10 cents used: %i\n", n10);
+    }
+    if(n5 >= 1)
+    {
+        printf("\n 5 cents used: %i\n", n5);
+    }
+    if(n1 >= 1)
+    {
+        printf("\n 1 cents used: %i\n", n1);
+    }
 }
